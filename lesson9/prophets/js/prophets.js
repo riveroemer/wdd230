bc7f3e39e8cc64e6a49355244de46c641eff112b
EmerRivero <riv21017@byui.edu>
2022-06-15 20:19:59 -0500

    upload and replace

--- lesson9/prophets/js/prophets.js
+++ lesson9/prophets/js/prophets.js
@@ -1,34 +1,46 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
+const cards = document.querySelector('cards');
 
 fetch(requestURL)
-  .then(function(respose){
-    return respose.json();
-  })
-  .then(function(jsonObject){
-    console.table(jsonObject);
-    const prophets = jsonObject['prophets'];
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+        const prophets = jsonObject['prophets'];
+        prophets.forEach(displayProphets);
+    });
 
-    for (let i = 0; i < prophets.length; i++) {
+function displayProphets(prophet) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let birthDate = document.createElement('p');
+    let birthPlace = document.createElement('p');
+    let portrait = document.createElement('img');
 
-      let card = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let p1 = document.createElement('p');
-      let p2 = document.createElement('p');
-      let img = document.createElement('img');
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
 
-      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-      p1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-      p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-      img.setAttribute('src', prophets[i].imageurl);
-      img.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + '-' + prophets[i].order);
+    // Change the textContent property of the p element to contain the birth date and birth place 
+    birthDate.textContent = `Date of Birth - ${prophet.birthdate}`
+    birthPlace.textContent= `Place of Birth - ${prophet.birthplace}`
 
-      card.appendChild(h2);
-      card.appendChild(p1);
-      card.appendChild(p2);
-      card.appendChild(img);
+    // Build the images attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropiate variable).
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th Latter-day President`);
+    portrait.setAttribute('loading', 'lazy');
 
-      document.querySelector('div.cards').appendChild(card);
-    }
-  });
+    //  Add/append the section(card) with the h2 element
+    card.appendChild(h2);
+    card.appendChild(birthDate);
+    card.appendChild(birthPlace);
+    card.appendChild(portrait);
 
-  
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+
+};
+
+
+
